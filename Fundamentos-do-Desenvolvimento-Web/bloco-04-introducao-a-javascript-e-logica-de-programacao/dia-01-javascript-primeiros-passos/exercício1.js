9fb1c154fcf27d7140d71277044ce560e270851b
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-02 19:24:36 -0300

    Exercícios 2, 3, 4, 5

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercício1.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercício1.js
@@ -1,3 +1,5 @@
+
+//Exercício 1
 const a = 5;
 const b = 2;
 //Adição
@@ -14,6 +16,45 @@ console.log(a / b);
 
 //Módulo
 console.log(a % b);
+
+//Exercício 2
+const num1 = 2
+const num2 = 3
+
+if (num1 > num2) {
+    console.log (num1)
+}
+else {
+    console.log (num2)
+}
+
+//Exercício 3
+const num1 = 4
+const num2 = 3
+const num3 = 2
+
+if (num1 > num2 && num1 > num3) {
+console.log(num1);
+}
+else if (num2 > num1 && num2 > num3){
+console.log(num2);
+}
+else {
+console.log(num3);
+}
+
+//Exercício 4
+const a = 5
+
+if  (a > 0){
+    console.log("Positivo");
+}
+else if (a < 0){
+    console.log("Negativo");
+}
+else {
+    console.log("zero");
+}
 
 
 
