802ab4fa148cd457d1c4f3230ae5d96bdabe2fc6
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-03 19:03:58 -0300

    Exercício 2

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
@@ -5,4 +5,17 @@ for (let index = 0; index < numbers.length; index += 1) {
 console.log(numbers[index]);
 }
 
+//Exercício 2
+for(let index = 0; index < numbers.length; index += 1){
+    let soma = 0
+   (soma + numbers[index]);
+}
+    console.log(soma)
 
+let soma = 0
+
+for (let i=0; i<teste.length;i++){
+    soma += teste[i]
+}
+
+console.log(soma)
