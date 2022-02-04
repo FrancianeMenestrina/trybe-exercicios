81fb73da32539dc93811d265981c0331d7adc001
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-04 09:42:48 -0300

    Exercício 3

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
@@ -1,21 +1,30 @@
 //Exercício 1
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 0; index < numbers.length; index += 1) {
+// console.log(numbers[index]);
+// }
+
+// Exercício 2
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1){
+//   soma += numbers[index];
+// }
+//     console.log(soma);
+
+
+//Exercício 3
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-console.log(numbers[index]);
+for (let index = 0; index < numbers.length; index += 1){
+   soma += numbers[index];
 }
+    let media = soma / numbers.length;
+    console.log(media);
 
-//Exercício 2
-for(let index = 0; index < numbers.length; index += 1){
-    let soma = 0
-   (soma + numbers[index]);
-}
-    console.log(soma)
 
-let soma = 0
-
-for (let i=0; i<teste.length;i++){
-    soma += teste[i]
-}
-
-console.log(soma)
