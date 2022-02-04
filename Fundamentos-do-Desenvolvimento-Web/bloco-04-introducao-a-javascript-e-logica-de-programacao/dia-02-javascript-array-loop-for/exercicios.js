9e9f021adb15aa28c8c559bf43ebf0be85969622
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-04 09:52:23 -0300

    Exercício 4

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
@@ -18,13 +18,31 @@
 
 //Exercício 3
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
 
-for (let index = 0; index < numbers.length; index += 1){
-   soma += numbers[index];
-}
-    let media = soma / numbers.length;
-    console.log(media);
+// for (let index = 0; index < numbers.length; index += 1){
+//    soma += numbers[index];
+// }
+//     let media = soma / numbers.length;
+//     console.log(media);
+
+//Exercício 4
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1){
+//    soma += numbers[index];
+// }
+//     let media = soma / numbers.length;
+//     console.log(media);
+
+//    if (media <=20){
+//       console.log("valor menor ou igual 20");
+//    } 
+//    else {
+//       console.log("valor maior que 20");
+//    }
 
 
