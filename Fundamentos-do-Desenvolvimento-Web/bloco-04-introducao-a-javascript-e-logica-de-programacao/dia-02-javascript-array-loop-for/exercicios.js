bd45e15ffc5e310e712ed91c9040208b53ca3072
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-05 16:07:30 -0300

    corrige exercício 7

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
@@ -78,15 +78,14 @@
 // Exercício 7
 
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// let menor = numbers [0]
+// let menor = numbers[0]
 
 // for (let index = 0; index < numbers.length; index += 1){
-
-// if (numbers[index] < menor) {
-//    menor = numbers[index];
-//  }
+//     if(numbers[index] < menor) {
+//         menor = numbers[index];
+//     }
 // }
-// console.log(menor);
+// console.log(menor + " é o menor número!");
 
 // Exercício 8
 
@@ -97,9 +96,9 @@
 // }
 // console.log(arrayNovo)
 
-Exercício 9
-let arrayNovo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+// Exercício 9
+// let arrayNovo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
-for (let index = 0; index < arrayNovo.length; index += 1){
-   console.log(arrayNovo[index] / 2)
-}
+// for (let index = 0; index < arrayNovo.length; index += 1){
+//    console.log(arrayNovo[index] / 2)
+// }
