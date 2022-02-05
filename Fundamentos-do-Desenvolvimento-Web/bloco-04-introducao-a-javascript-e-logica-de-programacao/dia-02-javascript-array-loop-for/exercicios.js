69be482b98a2097708f83167b291d33f6789b31d
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-05 17:11:19 -0300

    Exercício 9

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
@@ -97,8 +97,9 @@
 // console.log(listaNum);
 
 // Exercício 9
-// let arrayNovo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+// let divisao = [];
 
-// for (let index = 0; index < arrayNovo.length; index += 1){
-//    console.log(arrayNovo[index] / 2)
-// }
+// for (let index = 1; index <= numbers.length; index += 1){
+//     divisao.push(index/2);}
+// console.log(divisao);
