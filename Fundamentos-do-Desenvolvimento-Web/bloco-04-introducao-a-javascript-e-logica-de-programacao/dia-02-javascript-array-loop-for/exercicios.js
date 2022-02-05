ae9edd26af49892b1631b5524a0ada9f1c5090ea
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-05 19:07:26 -0300

    Bônus exercício 3

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/exercicios.js
@@ -133,3 +133,18 @@ http://devfuria.com.br/logica-de-programacao/exemplos-na-linguagem-c-do-algoritm
 //        }
 // };
 // console.log(vetor);
+
+Bônus Exercício 3
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let multiplicados = [];
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (index + 1 < numbers.length) {
+//       multiplicados.push(numbers[index] * numbers[index + 1]);}
+//       else {
+//           multiplicados.push(numbers[index] * 2);}
+//   }
+    
+
+// console.log(numbers); 
+// console.log(multiplicados);
