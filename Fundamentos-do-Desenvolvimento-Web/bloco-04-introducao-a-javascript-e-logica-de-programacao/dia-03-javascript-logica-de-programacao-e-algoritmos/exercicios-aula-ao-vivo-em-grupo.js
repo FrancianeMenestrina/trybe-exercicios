e7e002a9d0b8dc7c24b99ee105279381f231ec05
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-05 22:24:32 -0300

    Exercício aula ao vivo 01

---
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-aula-ao-vivo-em-grupo.js
@@ -0,0 +1,13 @@
+// Exercício 1
+// Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato:
+// A soma total de 1 a 50 é
+// let array = [];
+// let soma = 0;
+
+// for (let index = 0; index <= 50; index += 1){
+//     array.push(index)
+//     soma = soma + array[index];
+// }
+
+// console.log("A soma total de 1 a 50 é "+ soma);
+
