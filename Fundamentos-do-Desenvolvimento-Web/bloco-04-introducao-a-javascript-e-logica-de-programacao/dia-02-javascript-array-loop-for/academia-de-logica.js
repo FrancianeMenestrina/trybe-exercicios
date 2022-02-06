f50bd094ba7d05059d3e366bb1c99b715e589455
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-05 21:17:07 -0300

    Exercício 2 academia de lógica

---
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-loop-for/academia-de-logica.js
@@ -0,0 +1,29 @@
+// Exercício 1
+
+// let numerosDez = [];
+// let soma = 0;
+
+// for (let index = 0; index <=10; index += 1){
+// numerosDez.push(index);
+//     soma = soma + numerosDez[index];
+// }
+
+// console.log(numerosDez);
+
+// console.log(soma);
+
+// Exercício 2
+// let listaDora = [];
+// let impares = [];
+
+// for(let index = 0; index <= 50; index += 1){
+//     listaDora.push(index);
+//     if(listaDora[index] %2 !== 0) {
+//         impares.push(index);
+//     }
+// }
+// console.log(listaDora);
+// console.log(impares);
+// stringDora = impares.toString ();
+
+// console.log(stringDora);
