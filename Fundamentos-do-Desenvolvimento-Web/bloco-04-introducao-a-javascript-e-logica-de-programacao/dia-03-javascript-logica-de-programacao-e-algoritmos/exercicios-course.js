6bc912bd025f9723e045bf7e9075376ae70d50ae
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-07 10:36:10 -0300

    Exercício 2

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-course.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-course.js
@@ -1,6 +1,18 @@
-let fatorial = 10;
-let resultado = 1;
-for (let index = fatorial; index > 1; index -= 1) {
-    resultado = resultado * index;
+// Exercício 1
+// let fatorial = 10;
+// let resultado = 1;
+// for (let index = fatorial; index > 1; index -= 1) {
+//     resultado = resultado * index;
+// }
+// console.log(resultado)
+
+
+// Exercício 2
+let palavra = 'tryber';
+let invertePalavra = '';
+
+for (let index = 0; index < palavra.length; index += 1) {
+  invertePalavra = invertePalavra + palavra[palavra.length - 1 - index];
 }
-console.log(resultado)
+
+console.log(invertePalavra);
