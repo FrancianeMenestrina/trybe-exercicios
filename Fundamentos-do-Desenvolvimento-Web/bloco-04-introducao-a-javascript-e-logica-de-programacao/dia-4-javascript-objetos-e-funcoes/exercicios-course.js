88c27ac3eecdfd4d4ffadb5dd1c09380650a59b3
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-07 21:56:26 -0300

    Exercício 3

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-course.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-course.js
@@ -16,14 +16,19 @@
   
 //   console.log("Bem vinda, "+ info["personagem"] + " !");
 
-Exercício 2
-2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. 
+// Exercício 2
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. 
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
-  info["recorrente"] = "Sim";
-  console.log(info);
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+//   info["recorrente"] = "Sim";
+//   console.log(info);
   
+// Exercício 3
+// Faça um for/in que mostre todas as chaves do objeto.
+// for (let key in info){
+//     console.log(key);
+// }
