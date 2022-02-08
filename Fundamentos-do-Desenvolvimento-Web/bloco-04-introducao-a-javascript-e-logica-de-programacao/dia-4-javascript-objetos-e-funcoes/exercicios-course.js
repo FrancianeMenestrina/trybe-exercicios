e44a59e5f99becef37cacb669e63546ab2a07acb
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-07 21:38:17 -0300

    Exercício 1

---
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-course.js
@@ -0,0 +1,17 @@
+Exercício 1
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+  
+  1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+  
+  console.log("Bem vinda, "+ info["personagem"] + " !");
