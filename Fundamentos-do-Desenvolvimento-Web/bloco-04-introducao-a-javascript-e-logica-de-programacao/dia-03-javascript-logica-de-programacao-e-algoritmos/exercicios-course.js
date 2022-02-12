d2bf81a452b045bd362b2e0d9c05a064cfba1c0e
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-12 19:50:45 -0300

    Exercício 4

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-course.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-course.js
@@ -17,7 +17,8 @@ for (let index = 0; index < palavra.length; index += 1) {
 
 console.log(invertePalavra);
 
-Exercício 3
+// Exercício 3
+// Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 
@@ -38,3 +39,38 @@ for (let index = 0; index < array.length; index += 1) {
 
 console.log(maiorPalavra);
 console.log(menorPalavra);
+
+// Exercício 4
+// 4- Um número primo é aquele divisível apenas por 1 e por ele mesmo. 
+//Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
+let array = []
+let arrayprimos = []
+for (let index = 0; index <= 50; index += 1){
+    array.push(index)
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  let numero = array[index]
+  let count = 0;
+
+  for (let index2 = 0; index2 < array.length; index2 +=1) {
+    if (numero % array[index2] === 0){
+      count = count + 1
+    }
+  }
+
+  if (count == 2){
+    // é primo
+    arrayprimos.push(numero)
+  }
+}
+let ehmaiornumero = 0
+for (let index = 0; index < arrayprimos.length; index += 1){
+  let numero = arrayprimos[index]  
+  for (let index2 = 0; index2 < arrayprimos.length; index2 += 1){
+    if (numero > arrayprimos[index2]){
+      ehmaiornumero = numero
+    }
+  }
+}
+console.log(ehmaiornumero + " é o maior número primo");
