dee953395d1d7945eb12c6a444a416166901ceaf
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-13 14:21:48 -0300

    Exercício 06 Funções

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-course.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-course.js
@@ -223,3 +223,18 @@ for (let index = 0; index < array.length; index+=1) {
 return numerocommaiorcontador
 }
 console.log(contaNumerosRepetidos([2, 3, 2, 5, 8, 2, 3]))
+
+
+// 6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+// Valor de teste: N = 5 .
+// Valor esperado no retorno da função: 1+2+3+4+5 = 15 .
+
+
+function somaNumeros(N) {
+let somatorio = 0
+for (let index = 0; index <= N; index += 1){
+    somatorio = somatorio + index 
+}
+  return somatorio
+}
+console.log(somaNumeros(5));
