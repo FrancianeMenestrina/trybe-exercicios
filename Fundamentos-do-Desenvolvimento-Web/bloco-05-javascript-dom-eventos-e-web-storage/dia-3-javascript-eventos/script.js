7b4d591906f8194ee7c1e2703c8ea7dcf3c9a16c
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-13 18:28:48 -0300

    Exercício 01

--- Fundamentos-do-Desenvolvimento-Web/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,3 +14,20 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  let listaDias = document.querySelector("ul#days")
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  for (let index = 0; index < dezDaysList.length; index += 1){
+    let elementoLista = document.createElement("li")
+    elementoLista.classList.add("day")
+    elementoLista.innerText = dezDaysList[index]
+  
+    if(dezDaysList[index] == 24 || dezDaysList[index] == 25 || dezDaysList[index] == 31){
+      elementoLista.classList.add("holiday")
+    }
+    if(dezDaysList[index] == 4 || dezDaysList[index] == 11 || dezDaysList[index] == 18 || dezDaysList[index] == 25){
+        elementoLista.classList.add("friday")
+      }
+  
+    listaDias.appendChild(elementoLista)
+  }
+ 
