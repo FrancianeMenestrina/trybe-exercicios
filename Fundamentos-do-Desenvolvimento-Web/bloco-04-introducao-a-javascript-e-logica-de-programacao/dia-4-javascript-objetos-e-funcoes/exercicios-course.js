44ed5ed47954259019287736fa0b914179875104
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-13 10:42:42 -0300

    Exercício Funções 5

--- Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-course.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-course.js
@@ -188,3 +188,38 @@ function MaiorNome(arraynomes) {
 }
 
 console.log(MaiorNome(["Ivo", "Maria", "Pe"]))
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+// Array de teste: [2, 3, 2, 5, 8, 2, 3]; .
+// Valor esperado no retorno da função: 2 .
+
+// 2 3 2 5 8 2 3 
+
+// array[index] == array[index]
+// 2 == 2
+// 3 == 3
+// 2 == 2
+// 5 == 5
+// 8 == 8
+// 2 == 2
+// 3 == 3
+
+function contaNumerosRepetidos(array){
+let numerocommaiorcontador = 0
+for (let index = 0; index < array.length; index+=1) {
+  let contador = 0;
+
+  for (let index2 = 0; index2 < array.length; index2 +=1) {
+    if (array[index] === array[index2]){
+        contador = contador + 1
+    }
+  }
+  // console.log("original " + array[index] + " contador " + contador)
+  if (contador > numerocommaiorcontador){
+      numerocommaiorcontador = array[index]
+  }
+}
+  
+return numerocommaiorcontador
+}
+console.log(contaNumerosRepetidos([2, 3, 2, 5, 8, 2, 3]))
