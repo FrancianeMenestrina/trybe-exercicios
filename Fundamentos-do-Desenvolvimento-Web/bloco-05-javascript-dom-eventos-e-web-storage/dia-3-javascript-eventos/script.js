f4e8f61298dab49174869fceee12535911693ba0
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-02-13 21:19:49 -0300

    Exercício 05

--- Fundamentos-do-Desenvolvimento-Web/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,6 +14,12 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+//  Exercício 1 - O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+//   Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+//   Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+//   Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+
   let listaDias = document.querySelector("ul#days")
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   for (let index = 0; index < dezDaysList.length; index += 1){
@@ -31,6 +37,12 @@ function createDaysOfTheWeek() {
     listaDias.appendChild(elementoLista)
   }
  
+ 
+ 
+//   Exercício 2:
+//   Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+//   Adicione a este botão a ID "btn-holiday" .
+//   Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
   function criaFeriados (param) {
     let buttonsContainer = document.querySelector(".buttons-container")
     let button = document.createElement("button")
@@ -42,9 +54,13 @@ function createDaysOfTheWeek() {
 
   criaFeriados("Feriados")
 
-//   object.addEventListener("click", myScript);
 
-  
+
+  //   object.addEventListener("click", myScript);
+//   Exercício 3:
+// Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
+
 function mudaCorButton (){
     let hollydays = document.querySelectorAll("li.holiday")
     for (let index = 0; index < hollydays.length; index += 1){
@@ -61,6 +77,11 @@ let hollydayButton = document.querySelector("#btn-holiday")
 hollydayButton.addEventListener("click", mudaCorButton)
 
 
+
+// Exercício 4:
+// Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+// Adicione a este botão o ID "btn-friday" .
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
 function criaSextaFeira (param) {
     let buttonsContainer = document.querySelector(".buttons-container")
     let button = document.createElement("button")
@@ -68,4 +89,26 @@ function criaSextaFeira (param) {
     button.innerText = param
     buttonsContainer.appendChild(button)
   }
-  criaFeriados("Sexta-Feira")
+  criaSextaFeira("Sexta-Feira")
+
+  
+//   Exercício 5:
+// Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias.
+
+  function mudaTextoButton (){
+    let sextaFeiras = document.querySelectorAll("li.friday")
+    for (let index = 0; index < sextaFeiras.length; index += 1){
+        if(sextaFeiras[index].innerText == "Sextou!"){
+            sextaFeiras[index].innerText = sextaFeiras[index].getAttribute("data-original-value") //Aqui estamos guardando o valor anterior do atributo.
+            sextaFeiras[index].removeAttribute("data-original-value") //Aqui não precisamos mais do valor antigo nem do atributo, por isso estamos removendo, para deixar mais clean.
+        } else{
+            sextaFeiras[index].setAttribute("data-original-value", sextaFeiras[index].innerText)
+            sextaFeiras[index].innerText = "Sextou!"
+        }
+    }
+
+}
+
+let sextaFeirasButton = document.querySelector("#btn-friday")
+sextaFeirasButton.addEventListener("click", mudaTextoButton)
