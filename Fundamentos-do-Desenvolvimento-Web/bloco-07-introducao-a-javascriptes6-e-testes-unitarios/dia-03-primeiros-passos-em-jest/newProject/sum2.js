7d71709d4b87b74e7c6468c6ee2e866041982281
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-03-07 18:54:16 -0300

    Exercício 1.1

--- Fundamentos-do-Desenvolvimento-Web/bloco-07-introducao-a-javascriptes6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/newProject/sum2.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-07-introducao-a-javascriptes6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/newProject/sum2.js
@@ -1,8 +1,15 @@
+
+
 function sum(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') {
       throw new Error('parameters must be numbers');
     }
   
     return a + b;
-  }
-  console.log(sum(2, 2)=== 4);
+  };
+
+  module.exports = sum;
+  
+  
+  
+ 
---
+++ Fundamentos-do-Desenvolvimento-Web/bloco-07-introducao-a-javascriptes6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/newProject/sum2.test.js
@@ -0,0 +1,14 @@
+// const sum = require('./sum2');
+
+// test('Soma de 4 e 5 resulta em 9', () => {
+//     expect(sum(4, 5)).toBe(9);
+// });
+
+// ou 
+const sum = require('./sum2');
+
+describe('Requisito 1', () => {
+    it('Soma de 4 e 5 resulta em 9', () => {
+        expect(sum(4, 5)).toBe(9); 
+})
+})
