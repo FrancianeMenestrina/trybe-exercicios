c4f0e743553478a209287e34f5a740143a5d661c
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-03-07 20:06:18 -0300

    Exercício 1.3

--- Fundamentos-do-Desenvolvimento-Web/bloco-07-introducao-a-javascriptes6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/newProject/sum2.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-07-introducao-a-javascriptes6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/newProject/sum2.js
@@ -8,6 +8,7 @@ function sum(a, b) {
     return a + b;
   };
 
+    // console.log(sum(5, "5"));
   module.exports = sum;
   
   
--- Fundamentos-do-Desenvolvimento-Web/bloco-07-introducao-a-javascriptes6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/newProject/sum2.test.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-07-introducao-a-javascriptes6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/newProject/sum2.test.js
@@ -1,3 +1,5 @@
+
+// 1-Teste se o retorno de sum(4, 5) é 9
 // const sum = require('./sum2');
 
 // test('Soma de 4 e 5 resulta em 9', () => {
@@ -10,5 +12,20 @@ const sum = require('./sum2');
 describe('Requisito 1', () => {
     it('Soma de 4 e 5 resulta em 9', () => {
         expect(sum(4, 5)).toBe(9); 
+})
+})
+
+// 2-Teste se o retorno de sum(0, 0) é 0
+
+describe('Requisito 2', () => {
+    it('Soma de 0 e 0 resulta em 0', () => {
+        expect(sum(0, 0)).toBe(0); 
+})
+})
+
+// 3-Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+describe('Requisito 3', () => {
+    it('Soma de 4 e "5" exibe Erro', () => {
+        expect(() => {sum(4, "5") }).toThrow('parameters must be numbers'); 
 })
 })
