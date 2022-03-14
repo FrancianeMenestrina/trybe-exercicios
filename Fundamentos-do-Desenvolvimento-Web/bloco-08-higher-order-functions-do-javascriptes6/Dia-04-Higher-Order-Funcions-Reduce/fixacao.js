6504f01a67ca1d0722e2b726a0a4e473a208c549
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-03-14 16:17:29 -0300

    Exercício 1

---
+++ Fundamentos-do-Desenvolvimento-Web/bloco-08-higher-order-functions-do-javascriptes6/Dia-04-Higher-Order-Funcions-Reduce/fixacao.js
@@ -0,0 +1,19 @@
+
+// 1 - Dada uma matriz, transforme em um array.
+
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+  ];
+  
+  function flatten() {
+    return arrays.reduce(
+        (acumulator, valorAtual) => acumulator.concat(valorAtual),
+    []
+    )};
+  console.log(flatten());
+
+
+
+
