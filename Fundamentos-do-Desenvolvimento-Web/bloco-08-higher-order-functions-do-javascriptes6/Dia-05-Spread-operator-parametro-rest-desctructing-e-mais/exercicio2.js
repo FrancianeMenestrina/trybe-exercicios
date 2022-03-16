56391ad4fa579d45f943385cc81a75f4b9c62ff9
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-03-16 14:16:04 -0300

    Exercícios

---
+++ Fundamentos-do-Desenvolvimento-Web/bloco-08-higher-order-functions-do-javascriptes6/Dia-05-Spread-operator-parametro-rest-desctructing-e-mais/exercicio2.js
@@ -0,0 +1,8 @@
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+// Dica: use parâmetro rest .
+
+// function sum (...numbers) {
+//     let total = a + b;
+//     return total
+// };
+// console.log(sum(...numbers));
