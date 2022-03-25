d30b7a89d9371b06e5f771a0060f20e016e4cc30
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-03-25 11:34:40 -0300

    Complementos

--- Fundamentos-do-Desenvolvimento-Web/bloco-09-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/Exercise2/api.js
+++ Fundamentos-do-Desenvolvimento-Web/bloco-09-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/Exercise2/api.js
@@ -4,9 +4,9 @@ const fetchCoins = async () => {
     const url = 'https://api.coincap.io/v2/assets';
   
     const coins = await fetch(url)
-      .then((response) => response.json())
-      .then((data) => data.data)
-      .catch((error) => error.toString());
+    .then((response) => response.json())
+    .then((data) => data.data)
+    .catch((error) => error.toString());
   
     return coins;
   }
